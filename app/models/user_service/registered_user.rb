25f715be7a741f4480abcfa7573780d6bcda7b30
Arman <arman.zrb@gmail.com>
2020-09-14 12:23:44 +1000

    Rescue errors in user import

--- app/models/user_service/registered_user.rb
+++ app/models/user_service/registered_user.rb
@@ -10,40 +10,47 @@ module UserService
       end
 
       rows.each do |row|
-        next if row['RegisteredUserUUID'].blank? || row['Email'].blank?
+        begin
+          next if row['RegisteredUserUUID'].blank? || row['Email'].blank?
 
-        ru = RegisteredUser.find_or_initialize_by(uuid: row['RegisteredUserUUID'])
-        ru.email = row['Email'].downcase
-        ru.fields = row
-        ru.save!
+          ru = RegisteredUser.find_or_initialize_by(uuid: row['RegisteredUserUUID'])
+          ru.email = row['Email'].downcase
+          ru.fields = row
+          ru.save!
 
-        next if row['Suspended'].to_i == 1
+          next if row['Suspended'].to_i == 1
 
-        u = ::User.find_by(uuid: row['RegisteredUserUUID'])
+          u = ::User.find_by(uuid: row['RegisteredUserUUID'])
 
-        next if u.present?
+          next if u.present?
 
-        u = ::User.find_or_initialize_by(email: row['Email'].downcase)
+          u = ::User.find_or_initialize_by(email: row['Email'].downcase)
 
-        u.uuid = ru.uuid
-        u.full_name ||= (ru.fields['GivenName'].to_s + ' ' + ru.fields['Surname'].to_s).strip
-        u.roles << 'seller' unless u.is_seller? || u.is_buyer?
-        u.password = u.password_confirmation = SecureRandom.hex(32) unless u.persisted?
+          u.uuid = ru.uuid
+          u.full_name ||= (ru.fields['GivenName'].to_s + ' ' + ru.fields['Surname'].to_s).strip
+          u.roles << 'seller' unless u.is_seller? || u.is_buyer?
+          u.password = u.password_confirmation = SecureRandom.hex(32) unless u.persisted?
 
-        abn = row['ABN']
-        if abn.present? && ABN.valid?(abn)
-          abn = ABN.new(abn).to_s
-          seller_id = ::SellerVersion.where(abn: abn, state: ['approved','pending']).first&.seller_id
-          seller_id ||= ::SellerVersion.where(abn: abn, next_version_id: nil).first&.seller_id
+          abn = row['ABN']
+          if abn.present? && ABN.valid?(abn)
+            abn = ABN.new(abn).to_s
+            seller_id = ::SellerVersion.where(abn: abn, state: ['approved','pending']).first&.seller_id
+            seller_id ||= ::SellerVersion.where(abn: abn, next_version_id: nil).first&.seller_id
 
-          if seller_id.present? && u.seller_id != seller_id
-            u.seller_id ||= seller_id
-            u.seller_ids |= [seller_id]
+            if seller_id.present? && u.seller_id != seller_id
+              u.seller_id ||= seller_id
+              u.seller_ids |= [seller_id]
+            end
           end
-        end
 
-        u.skip_confirmation_notification!
-        u.save!
+          u.skip_confirmation_notification!
+          u.save!
+        rescue => e
+          Airbrake.notify_sync(e.message, {
+            RUUUID: row['RegisteredUserUUID'],
+            trace: e.backtrace[0..5],
+          })
+        end
       end
     end
   end
