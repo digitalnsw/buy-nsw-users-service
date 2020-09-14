fa06dd1d7eba8755dcbe350f3892c73f75359525
Arman <arman.zrb@gmail.com>
2020-09-14 20:19:37 +1000

    Tenders invitation job

---
+++ app/jobs/user_service/tender_users_invite_job.rb
@@ -0,0 +1,30 @@
+module UserService
+  class TenderUsersInviteJob < SharedModules::SlackReportingJob
+    def perform
+      reminded = 0
+      now = Time.now.getlocal('+11:00')
+      if now.wday.in?(1..5) && now.hour.in?(9..14)
+        User.where.not(uuid: nil).
+             where(confirmed_at: nil, opted_out: false, suspended: false).
+             order(:confirmation_sent_at).limit(24).each do |user|
+
+          next if user.confirmation_sent_at > 4.weeks.ago
+
+          user.update_columns(confirmation_token: SecureRandom.base58(20), confirmation_sent_at: Time.now)
+
+          mailer = SellerInvitationMailer.with(user: user)
+          mailer.seller_invitation_email.deliver_later
+
+          reminded += 1
+        end
+      end
+
+      [
+        {
+          title: "Users who have not started their application, receive reminders every 28 days",
+          value: "#{reminded} reminders sent",
+        },
+      ]
+    end
+  end
+end
