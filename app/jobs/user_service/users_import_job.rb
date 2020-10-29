2ac40b7b3fa32a36390177b7f3aefbe92092aaff
Arman Sarrafi <arman.zrb@gmail.com>
2020-10-29 11:30:54 +1100

    enable ru import

--- app/jobs/user_service/users_import_job.rb
+++ app/jobs/user_service/users_import_job.rb
@@ -4,8 +4,8 @@ module UserService
       file = download_file(document)
       xml_doc = Nokogiri::XML(File.open(file))
       # Next two lines are commented, to prevent against accidental push of registered users
-      # UserService::RegisteredUser.import(xml_doc)
-      # document.update_attributes!(after_scan: nil)
+      UserService::RegisteredUser.import(xml_doc)
+      document.update_attributes!(after_scan: nil)
     end
   end
 end
